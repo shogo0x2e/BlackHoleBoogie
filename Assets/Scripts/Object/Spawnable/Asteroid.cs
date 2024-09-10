a6047996a57f1b7b6b292f248083b1f9bae02e83
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-09-10 21:42:38 +0200

    AbstactObject + Exploding Astroids :) + Astronaut / Spawning Logic

---
+++ Assets/Scripts/Object/Spawnable/Asteroid.cs
@@ -0,0 +1,42 @@
+﻿using Object.Manager;
+using UnityEngine;
+
+namespace Object.Spawnable {
+    public class Asteroid : AbstractObject {
+        public AsteroidManager asteroidManager;
+
+        private GameObject currentModel;
+        private bool broken = false;
+
+        protected override void OnSpawn() {
+            GameObject selectedModel =
+                asteroidManager.asteroidModels[Random.Range(0, asteroidManager.asteroidModels.Length)];
+            currentModel = Instantiate(selectedModel, transform.position, transform.rotation);
+            currentModel.transform.parent = transform;
+        }
+
+        public void Explode() {
+            if (broken) {
+                return;
+            }
+
+            GameObject repl = Instantiate(currentModel, transform.position, transform.rotation);
+
+            Transform[] allChildren = repl.GetComponentsInChildren<Transform>();
+            foreach (Transform child in allChildren) {
+                if (child.GetComponent<Rigidbody>() == null) {
+                    child.gameObject.AddComponent<Rigidbody>();
+                }
+            }
+
+            Rigidbody[] rbs = repl.GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody rb in rbs) {
+                rb.AddExplosionForce(100, transform.position, 2);
+            }
+
+            broken = true;
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Object/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using Object.Manager;
+using UnityEngine;
+using Utils;
+
+namespace Object {
+    public class Spawner : MonoBehaviour {
+        public SpawnableManager spawnableManager;
+
+        private const float xzRange = 10F;
+        private const float y = 0F;
+
+        public void FixedUpdate() {
+            if (Random.Range(0F, 1F) < 0.02F) {
+                SpawnRandomObject();
+            }
+        }
+
+        private void SpawnRandomObject() {
+            AbstractObject rdObject = spawnableManager.objects[Random.Range(0, spawnableManager.objects.Length)];
+            Instantiate(rdObject, Vector.GetRandomPosition(xzRange, y, xzRange), Quaternion.identity);
+        }
+    }
+}
