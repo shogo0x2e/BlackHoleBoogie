568c33e26e6e3a415c22e713b8a3cd55efb1b94b
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-09-14 21:34:38 +0200

    Better Audio Dispawn

--- Assets/Scripts/Utils/AudioManager.cs
+++ Assets/Scripts/Utils/AudioManager.cs
@@ -4,20 +4,11 @@ using UnityEngine;
 namespace Utils {
     public abstract class AudioManager {
         public static void PlayAudioSource(AudioSource audioSource, Transform transform) {
-            AudioSource audioSourceInstance = UnityEngine.Object.Instantiate(
-                audioSource,
-                transform.position,
-                transform.rotation);
+            AudioSource audioSourceInstance =
+                UnityEngine.Object.Instantiate(audioSource, transform.position, transform.rotation);
             audioSourceInstance.Play();
 
-            // Use BlackHole instance since it will always exist
-            BlackHole.GetInstance().StartCoroutine(DestroyAfterPlaying(audioSourceInstance));
-        }
-
-        private static IEnumerator DestroyAfterPlaying(AudioSource audioSource) {
-            yield return new WaitForSeconds(audioSource.clip.length);
-
-            UnityEngine.Object.Destroy(audioSource.gameObject);
+            UnityEngine.Object.Destroy(audioSourceInstance.gameObject, audioSourceInstance.clip.length);
         }
     }
 }
