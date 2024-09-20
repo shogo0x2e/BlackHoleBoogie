3a5ad842be85867c77b6525f9a941e90489b2505
MercuriaL01 <iley.alvarez.funcke@gmail.com>
2024-09-20 16:12:43 +0200

    started working on leaderboard
    started working on leaderboard not jut the virtual keyboard

---
+++ Assets/Scenes/Test/Iley/HighscoreTable.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        float templateHeight = 20f; 
+        for(int i = 0; i < 10; i++)
+        {
+            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
+            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
+            entryTransform.gameObject.SetActive(true);
+        } 
+    }
+}
