2c28261eee8581350738d362de3ecac96104295a
PyroWilDx <pyrowildx314.kqm@gmail.com>
2024-09-26 23:29:28 +0200

    Reset tutorial when new game

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -13,6 +13,8 @@ public class TimeManager : MonoBehaviour {
 
         if (secondsLeft <= 0) {
             ScoreManager.scoreCount = 0;
+            TutorialManager.tutorialStep = 0;
+            TutorialManager.tutorialStringText = "Quest: Punch an Asteroid!";
             secondsLeft = gameDuration;
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
